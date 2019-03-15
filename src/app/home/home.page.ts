9096a817bd3fd94b72c25c7eb119e96d533c8cf7
k4ren <kkhakhis2@gmail.com>
2019-03-15 13:52:49 -0300

    error show picture

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  public image: any
+  
+  constructor(
+    private camera: Camera
+  ) { }
 
   openCamera(){
     console.log('here!');
-  }  
+    const options: CameraOptions = {
+      quality: 10,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+       //Set the source of the picture
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      //Rotate the image to correct for the orientation of the device during capture
+      correctOrientation: true 
+    }
+     this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+
+     this.image = imageData;
+
+     console.log(this.image);
+    //  let base64Image = 'data:image/jpeg;base64,' + imageData;
+    //  console.log(base64Image);
+     
+    }, (err) => {
+     // Handle error
+    });
+  }
 
   ngOnInit() {
     console.log('welcome');
