54bdf8a21899479d3866986b5f97a620940a15db
k4ren <kkhakhis2@gmail.com>
2019-03-21 16:09:35 -0300

    add function create new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Camera } from '@ionic-native/camera/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import {File} from '@ionic-native/file/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    File,
+    FilePath,
     Camera,
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -8,31 +11,50 @@ import { WebView } from '@ionic-native/ionic-webview/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  public image: any;
-  
+  public image: string;
+  public imgCopy: string;
+  public imagenes = [];
+
   constructor(
     private camera: Camera,
-    private webview: WebView
+    private webview: WebView,
+    private file: File,
+    private filePath: FilePath
   ) { }
 
   openCamera(){
     const options: CameraOptions = {
-      quality: 10,
+      quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
        //Set the source of the picture
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       //Rotate the image to correct for the orientation of the device during capture
-      correctOrientation: true 
+      correctOrientation: true
     }
     this.camera.getPicture(options).then((imageData) => {
-        this.image = this.webview.convertFileSrc(imageData);
+      this.image = this.webview.convertFileSrc(imageData);
     }, (err) => {
      // Handle error
      console.log(err);
-    });
-  }  
+    }); 
+  }
+  createNewFile() {
+    console.log('here!'); 
+  // https://github.com/ionic-team/ionic-native/issues/657#issuecomment-253084301
+    this.file.resolveDirectoryUrl(this.file.cacheDirectory).then((dirEntry) => {
+      dirEntry.getFile('example.jpeg', {create: true, exclusive: false}, (fileEntry) => {
+        console.log(fileEntry);
+      }, (err) => {
+        console.log(err);        
+      });
+    }, (err) => {
+      console.log(err);      
+    })
+
+  }
+ 
 
   ngOnInit() {
     console.log('welcome');
